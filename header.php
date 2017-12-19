616fa10ecbca163412102f4a87d5cf101056ea3f
Vovchyk <vova.senyk@gmail.com>
2017-12-19 10:03:18 +0200

    Initial commit

---
+++ header.php
@@ -0,0 +1,30 @@
+<?php
+header('Expires: '.gmdate('D, d M Y H:i:s \G\M\T', time() + 3600));
+header('Content-Type: text/html; charset=utf-8');
+header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
+header('X-UA-Compatible: IE=Edge,chrome=1');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+    <title><?php seo_title(); ?></title>
+    <meta name="MobileOptimized" content="width" />
+    <meta name="HandheldFriendly" content="True"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimum-scale=1.0, maximum-scale=1.0"/>
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+    <?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+<div id="main">
+    <header>
+        <div class="container">
+            <a id="logo" href="<?php echo get_option('home') ?>">
+                <img src="<?php echo theme('img/logo.png'); ?>" alt="logo">
+            </a>
+            <div id="menuOpen"><p>Menu</p><span></span></div>
+            <nav id="mainMenu">
+                <?php wp_nav_menu(array('container' => false, 'items_wrap' => '<ul id="%1$s">%3$s</ul>', 'theme_location'  => 'main_menu')); ?>
+            </nav>
+        </div>
+    </header>
---
+++ js/logic.js
@@ -0,0 +1,39 @@
+$ = jQuery;
+$(document).ready(function () {
+    var width = document.body.clientWidth;
+
+    $("#menuOpen").click(function (e) {
+        $(this).toggleClass("opened");
+    });
+
+    if (width <= 1024) {
+        $("#mainMenu .menu-item-has-children > a").append("<span></span>");
+        $("#mainMenu .menu-item-has-children span").click(function() {
+            $(this).parent().next().slideToggle(300);
+            $(this).toggleClass("active");
+            return false;
+        });
+    }
+
+    /*var swiper = new Swiper('.swiper-container', {
+        pagination: '.swiper-pagination',
+        paginationClickable: true,
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev',
+        spaceBetween: 30
+    });*/
+
+    //WPCF7
+    $(this).on('click', '.wpcf7-not-valid-tip', function () {
+        $(this).prev().trigger('focus');
+        $(this).fadeOut(500, function () {
+            $(this).remove();
+        });
+    });
+
+    $(window).bind("resize", function () {
+
+    });
+
+
+});
