bc668564cab4addd7deb983976dbdb5e46ad5a1c
Vovchyk <vova.senyk@gmail.com>
2018-03-20 17:21:53 +0200

    upd

--- functions.php
+++ functions.php
@@ -13,9 +13,6 @@ add_action('acf/init', 'my_acf_init');
 
 function style_js()
 {
-    if (is_page_template("tpl-contact.php") || is_front_page()) {
-        wp_enqueue_script('gmap', "//maps.googleapis.com/maps/api/js?v=3.exp&language=en&key=AIzaSyAOfuigGs3rWzZsdcWmQTeGU82RGccVqfc", '', null);
-    }
     wp_enqueue_script('lib', get_template_directory_uri() . '/js/lib.js', array('jquery'), '1.0', true);
     wp_enqueue_script('logic', get_template_directory_uri() . '/js/logic.js', array('jquery'), '1.0', true);
     wp_enqueue_style('fonts', get_template_directory_uri() . '/style/fonts.css');
