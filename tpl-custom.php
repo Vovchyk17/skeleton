e07200ca90c14419851e1dbf3b7ab3559d05882b
Vovchyk <vova.senyk@gmail.com>
2018-04-08 14:09:53 +0300

    upd

--- tpl-custom.php
+++ tpl-custom.php
@@ -1,9 +1,7 @@
 <?php get_header(); /* Template Name: Example */ ?>
     <section class="content container">
-        <article>
-            <?php if ( have_posts() ) : while ( have_posts() ) : the_post();
-                the_content();
-            endwhile; endif; ?>
-        </article>
+        <?php if (have_posts()) : while (have_posts()) : the_post();
+            the_content();
+        endwhile; endif; ?>
     </section>
 <?php get_footer(); ?>
