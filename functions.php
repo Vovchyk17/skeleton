9235f8ef0ee69104d94a3c927b7c785ca16c7772
Vovchyk <vova.senyk@gmail.com>
2018-07-06 12:55:43 +0300

    upd

--- functions.php
+++ functions.php
@@ -13,6 +13,12 @@ add_action('acf/init', 'my_acf_init');
 
 function style_js()
 {
+    if ( ! is_admin() ) {
+        wp_deregister_script( 'jquery' );
+        wp_register_script( 'jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js' );
+        wp_enqueue_script( 'jquery' );
+    };
+
     wp_enqueue_script('lib', get_template_directory_uri() . '/js/lib.js', array('jquery'), '1.0', true);
     wp_enqueue_script('logic', get_template_directory_uri() . '/js/logic.js', array('jquery'), '1.0', true);
     wp_enqueue_style('fonts', get_template_directory_uri() . '/style/fonts.css');
