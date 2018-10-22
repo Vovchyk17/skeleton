d138bc2e3d0f090098e58402184a1837f21de4e1
Vovchyk <vova.senyk@gmail.com>
2018-10-22 15:49:20 +0300

    upd

--- header.php
+++ header.php
@@ -13,6 +13,7 @@ header('X-UA-Compatible: IE=Edge');
     <meta name="HandheldFriendly" content="True"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimum-scale=1.0, maximum-scale=1.0"/>
     <?php wp_head(); ?>
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
 </head>
 <body <?php body_class(); ?>>
 <div id="main">
--- tpl-custom.php
+++ tpl-custom.php
@@ -1,7 +1,9 @@
 <?php get_header(); /* Template Name: Example */ ?>
-    <section class="content container">
-        <?php if (have_posts()) : while (have_posts()) : the_post();
-            the_content();
-        endwhile; endif; ?>
+    <section class="content">
+        <div class="container">
+	        <?php if (have_posts()) : while (have_posts()) : the_post();
+		        the_content();
+	        endwhile; endif; ?>
+        </div>
     </section>
 <?php get_footer(); ?>
