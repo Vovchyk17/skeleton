0ab2608a2be93da1f57d1652e8be29eec06fcfd2
Volodymyr Senyk <vova.senyk@gmail.com>
2020-04-22 20:17:00 +0300

    full upd

--- front-page.php
+++ front-page.php
@@ -4,6 +4,7 @@
         <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
             <?php the_content(); ?>
         <?php endwhile; endif; ?>
+        <a href="#" class="button">Button</a>
     </div>
 </section>
 <?php get_footer(); ?>
